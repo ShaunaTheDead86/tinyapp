06ba0d5eb420a275d38afe44e26013a40a67e085
Shauna Empringham <shaunathedead86@gmail.com>
2022-01-24 09:00:07 -0500

    adding ejs to express_server

--- express_server.js
+++ express_server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set('view engine', 'esj');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
+  const a = 1;
   res.send("Hello!");
 });
 
@@ -16,5 +19,10 @@ app.listen(PORT, () => {
 });
 
 app.get("/urls.json", (req, res) => {
+  console.log(a);
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
